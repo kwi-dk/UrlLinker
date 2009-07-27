ad6e3a7774aa81400676cafab8f349c9ee4d11cd
Søren Løvborg <kwi@kwi.dk>
2009-07-27 17:36:00 +0200

    Add TLD check and proper detection of URL boundaries..

--- UrlLinker.php
+++ UrlLinker.php
@@ -14,24 +14,19 @@
  *  Regular expression bits used by htmlEscapeAndLinkUrls() to match URLs.
  */
 $rexProtocol  = '(https?://)?';
-$rexDomain    = '((?:[-a-zA-Z0-9]{1,63}\.)+[-a-zA-Z0-9]{2,63}|(?:[0-9]+\.){3}[0-9]+)';
+$rexDomain    = '((?:[-a-zA-Z0-9]{1,63}\.)+[-a-zA-Z0-9]{2,63}|(?:[0-9]{1,3}\.){3}[0-9]{1,3})';
 $rexPort      = '(:[0-9]{1,5})?';
 $rexPath      = '(/[!$-/0-9:;=@_\':;!a-zA-Z\x7f-\xff]*?)?';
 $rexQuery     = '(\?[!$-/0-9:;=@_\':;!a-zA-Z\x7f-\xff]+?)?';
 $rexFragment  = '(#[!$-/0-9:;=@_\':;!a-zA-Z\x7f-\xff]+?)?';
 $rexUrlLinker = "{\\b$rexProtocol$rexDomain$rexPort$rexPath$rexQuery$rexFragment(?=[?.!,;:\"]?(\s|$))}";
 
-function callback($match)
-{
-    if (!$match[1]) $match[1] = 'http://';
-    if (!$match[3]) $match[3] = '';
-    if (!$match[4]) $match[4] = '';
-    if (!$match[5]) $match[5] = '';
-    if (!$match[6]) $match[6] = '';
-
-    return '<a href="' . $match[1] . $match[2] . $match[3] . $match[4] . $match[5] . $match[6] . '">'
-        . $match[2] . $match[3] . $match[4] . '</a>';
-}
+/**
+ *  $validTlds is an associative array mapping valid TLDs to the value true.
+ *  Since the set of valid TLDs is not static, this array should be updated
+ *  from time to time.
+ */
+$validTlds = array_fill_keys(explode(" ", ".ac .ad .ae .aero .af .ag .ai .al .am .an .ao .aq .ar .arpa .as .asia .at .au .aw .ax .az .ba .bb .bd .be .bf .bg .bh .bi .biz .bj .bm .bn .bo .br .bs .bt .bv .bw .by .bz .ca .cat .cc .cd .cf .cg .ch .ci .ck .cl .cm .cn .co .com .coop .cr .cu .cv .cx .cy .cz .de .dj .dk .dm .do .dz .ec .edu .ee .eg .er .es .et .eu .fi .fj .fk .fm .fo .fr .ga .gb .gd .ge .gf .gg .gh .gi .gl .gm .gn .gov .gp .gq .gr .gs .gt .gu .gw .gy .hk .hm .hn .hr .ht .hu .id .ie .il .im .in .info .int .io .iq .ir .is .it .je .jm .jo .jobs .jp .ke .kg .kh .ki .km .kn .kp .kr .kw .ky .kz .la .lb .lc .li .lk .lr .ls .lt .lu .lv .ly .ma .mc .md .me .mg .mh .mil .mk .ml .mm .mn .mo .mobi .mp .mq .mr .ms .mt .mu .museum .mv .mw .mx .my .mz .na .name .nc .ne .net .nf .ng .ni .nl .no .np .nr .nu .nz .om .org .pa .pe .pf .pg .ph .pk .pl .pm .pn .pr .pro .ps .pt .pw .py .qa .re .ro .rs .ru .rw .sa .sb .sc .sd .se .sg .sh .si .sj .sk .sl .sm .sn .so .sr .st .su .sv .sy .sz .tc .td .tel .tf .tg .th .tj .tk .tl .tm .tn .to .tp .tr .travel .tt .tv .tw .tz .ua .ug .uk .us .uy .uz .va .vc .ve .vg .vi .vn .vu .wf .ws .xn--0zwm56d .xn--11b5bs3a9aj6g .xn--80akhbyknj4f .xn--9t4b11yi5a .xn--deba0ad .xn--g6w251d .xn--hgbk6aj7f53bba .xn--hlcj6aya9esc7a .xn--jxalpdlp .xn--kgbechtv .xn--zckzah .ye .yt .yu .za .zm .zw"), true);
 
 /**
  *  Transforms plain text into valid HTML, escaping special characters and
@@ -39,6 +34,45 @@ function callback($match)
  */
 function htmlEscapeAndLinkUrls($text)
 {
-    global $rexUrlLinker;
-    return preg_replace_callback($rexUrlLinker, 'callback', htmlspecialchars($text));
+    global $rexUrlLinker, $validTlds;
+
+    $result = "";
+
+    $position = 0;
+    while (preg_match($rexUrlLinker, $text, $match, PREG_OFFSET_CAPTURE, $position))
+    {
+        list($url, $urlPosition) = $match[0];
+
+        // Add the text leading up to the URL.
+        $result .= htmlspecialchars(substr($text, $position, $urlPosition - $position));
+
+        $domain = $match[2][0];
+        $port   = $match[3][0];
+        $path   = $match[4][0];
+
+        // Check that the TLD is valid or that $domain is an IP address.
+        $tld = strtolower(strrchr($domain, '.'));
+        if (preg_match('{^\.[0-9]{1,3}$}', $tld) || isset($validTlds[$tld]))
+        {
+            // Prepend http:// if no protocol specified
+            $completeUrl = $match[1][0] ? $url : "http://$url";
+
+            // Add the hyperlink.
+            $result .= '<a href="' . htmlspecialchars($completeUrl) . '">'
+                . htmlspecialchars("$domain$port$path")
+                . '</a>';
+        }
+        else
+        {
+            // Not a valid URL.
+            $result .= htmlspecialchars($url);
+        }
+
+        // Continue text parsing from after the URL.
+        $position = $urlPosition + strlen($url);
+    }
+
+    // Add the remainder of the text.
+    $result .= htmlspecialchars(substr($text, $position));
+    return $result;
 }
