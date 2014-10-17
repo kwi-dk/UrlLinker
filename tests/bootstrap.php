4f441222a6cf70a0ee0545f45fb36acacabc381d
Dawid Nowak <code@dnowak.pl>
2014-10-17 21:23:26 +0200

    Composer: package configuration + autoloading

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,9 +3,14 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-require_once __DIR__.'/../UrlLinker.php';
-require_once __DIR__.'/../lib/UrlLinkerInterface.php';
-require_once __DIR__.'/../lib/UrlLinker.php';
-require_once __DIR__.'/UrlLinkerTestCase.php';
-require_once __DIR__.'/functions/LinkUrlsInTrustedHtmlTest.php';
-require_once __DIR__.'/functions/HtmlEscapeAndLinkUrlsTest.php';
+function includeIfExists($file)
+{
+    return file_exists($file) ? include $file : false;
+}
+
+if (!includeIfExists(__DIR__.'/../vendor/autoload.php')) {
+    echo 'You must set up the project dependencies, run the following commands:'.PHP_EOL.
+        'curl -sS https://getcomposer.org/installer | php'.PHP_EOL.
+        'php composer.phar install'.PHP_EOL;
+    exit(1);
+}
